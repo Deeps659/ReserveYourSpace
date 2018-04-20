1ba38d406f6d9427d1eb7a2a5565f301bfa19724
Deepali Maheshwari <deepali.maheshwari@blackrock.com>
2018-04-20 17:40:13 +0530

    add homeviewcontroller

---
+++ reserveyourspace-objectivec/ReserveYourSpace/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  ReserveYourSpace
+//
+//  Created by Maheshwari, Deepali on 20/4/18.
+//  Copyright © 2018 Singh, Navin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ reserveyourspace-objectivec/ReserveYourSpace/HomeViewController.m
@@ -0,0 +1,98 @@
+//
+//  HomeViewController.m
+//  ReserveYourSpace
+//
+//  Created by Maheshwari, Deepali on 20/4/18.
+//  Copyright © 2018 Singh, Navin. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface HomeViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@property (strong, nonatomic) IBOutlet UIView *scannerView;
+@property (strong, nonatomic) IBOutlet UIButton *scanButton;
+@property (weak, nonatomic) IBOutlet UILabel *response;
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)startScanning:(id)sender {
+    [self startReading];
+}
+
+-(BOOL)startReading {
+    NSError *error;
+    
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    if (!input) {
+        NSLog(@"%@", [error localizedDescription]);
+        return NO;
+    }
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    [_captureSession addInput:input];
+    
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [_captureSession addOutput:captureMetadataOutput];
+    
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeQRCode]];
+    
+    _videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    [_videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [_videoPreviewLayer setFrame:_scannerView.layer.bounds];
+    [_scannerView.layer addSublayer:_videoPreviewLayer];
+    
+    [_captureSession startRunning];
+    
+    return YES;
+}
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeQRCode]) {
+            [_response performSelectorOnMainThread:@selector(setText:) withObject:[metadataObj stringValue] waitUntilDone:NO];
+            
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:nil waitUntilDone:NO];
+            [_scanButton performSelectorOnMainThread:@selector(setTitle:) withObject:@"Start!" waitUntilDone:NO];
+            //   _isReading = NO;
+        }
+    }
+}
+
+-(void)stopReading{
+    [_captureSession stopRunning];
+    _captureSession = nil;
+    
+    [_videoPreviewLayer removeFromSuperlayer];
+}
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
+
